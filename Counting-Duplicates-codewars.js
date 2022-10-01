79b5b44a255b4c7fe650045d687327959d60677c
Nikita Gromov <105276303+Gromov812@users.noreply.github.com>
2022-10-01 13:52:14 +0500

    Update Counting-Duplicates-codewars.js

--- Counting-Duplicates-codewars.js
+++ Counting-Duplicates-codewars.js
@@ -4,26 +4,23 @@ let inputString = "Indivisibilities";
 
 function func (text) {
 
-    text = text.toLowerCase();
-    let counter = {};
+    text = text.toLowerCase(); // transform argument string to lower case
+    let counter = {}; // initialize new Object to collect counting letters
 
-    text = [...text];
-    let resCount = 0;
-    console.log(text);
+    text = [...text]; // convert string to array
+    let resCount = 0; // initialize new var for return result 
+
+    //Do iterations to get Object counter with numbers of duplicates
+    
     for (let el in text) {
         counter[text[el]] = (counter[text[el]]+1) || 1;
-        
-        
-        
-    }
-
+ }
+    //Do another cicle for counter object for to get number of duplivctes  
         for (let el in counter) {
             console.log(counter[el]);
             if (counter[el] > 1) resCount++;
         }
 
-        return resCount;
+        return resCount;     // return result
 
 }
-
-console.log(func(inputString));
